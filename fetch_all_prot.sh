a0bb0306b3e014ac360760da95304aeee543a590
Maya Bose <47787632+boseHere@users.noreply.github.com>
2021-05-08 13:22:54 -0700

    Added header with script purpose for documentation

--- fetch_all_prot.sh
+++ fetch_all_prot.sh
@@ -1,3 +1,7 @@
+# Author: Maya Bose
+# Purpose: This script takes a list of species ($1) and the name of a protein ($2)
+# and returns all protein sequences containing that protein name available for 
+# each species of fish from the NCBI Protein database. 
 touch $2
 rm $2
 i=0
